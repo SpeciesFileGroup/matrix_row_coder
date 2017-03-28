a7eb2921c884af0b8b721409c12adc91fb4a4f67
Brian Walters <brianw@pixotech.com>
2017-03-28 15:36:08 -0500

    Flesh out descriptors

--- src/MatrixRowCoder/MatrixRowCoder.vue
+++ src/MatrixRowCoder/MatrixRowCoder.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="matrix-row-coder">
         <h1 class="matrix-row-coder__title">{{ title }}</h1>
-        <div>
-
-        </div>
+        <ul class="matrix-row-coder__descriptor-menu">
+            <li v-for="descriptor in descriptors">
+                <div>{{ descriptor.descriptor_tag }}</div>
+            </li>
+        </ul>
+        <ul class="matrix-row-coder__descriptor-list">
+            <li
+                class="matrix-row-coder__descriptor-container"
+                v-for="descriptor in descriptors">
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -14,7 +22,8 @@
     const ActionNames = require('../store/actions/actions').ActionNames;
 
     const computed = vuex.mapState({
-        title: state => state.taxonTitle
+        title: state => state.taxonTitle,
+        descriptors: state => state.descriptors
     });
 
     module.exports = {
