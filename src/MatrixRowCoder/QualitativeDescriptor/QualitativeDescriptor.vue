1ac52c281e94e8d5f86348feacd1e71ecd4bc9ea
Brian Walters <brianw@pixotech.com>
2017-03-30 13:09:10 -0500

    build basic qualitative descriptor

--- src/MatrixRowCoder/QualitativeDescriptor/QualitativeDescriptor.vue
+++ src/MatrixRowCoder/QualitativeDescriptor/QualitativeDescriptor.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
-        Qualitative Descriptor
-    </div>
+    <ul class="qualitative-descriptor">
+        <li v-for="characterState in descriptor.characterStates">
+            <label>
+                <input type="checkbox" />
+                {{ characterState.label }}: {{ characterState.name }}
+            </label>
+        </li>
+    </ul>
 </template>
+
+<style src="QualitativeDescriptor.styl" lang="stylus"></style>
 
 <script>
     module.exports = {
