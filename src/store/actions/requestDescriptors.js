7e919bd553dad33e8922b49d01caf7850daf3cd2
Brian Walters <brianw@pixotech.com>
2017-03-30 10:56:14 -0500

    begin transforming descriptors for use in Vue

--- src/store/actions/requestDescriptors.js
+++ src/store/actions/requestDescriptors.js
@@ -4,8 +4,31 @@ const MutationNames = require('../mutations/mutations').MutationNames;
 module.exports = function({ commit }) {
     return mockRequest.getDescriptors()
         .then(response => {
-            commit(MutationNames.SetDescriptors, response.descriptors);
+            commit(MutationNames.SetDescriptors, response.descriptors.map(transformDescriptorForViewmodel));
             commit(MutationNames.SetTaxonId, response.otu_id);
             commit(MutationNames.SetTaxonTitle, response.otu_tag);
         });
 };
+
+function transformDescriptorForViewmodel(descriptorData) {
+    return {
+        componentName: getComponentNameForDescriptorType(descriptorData),
+        title: descriptorData.descriptor_tag,
+        description: getDescription(descriptorData)
+    };
+}
+
+const ComponentNames = {
+    Qualitative: `qualitative-descriptor`,
+    Presence: `presence-descriptor`,
+    Continuous: `continuous-descriptor`,
+    Sample: `sample-descriptor`
+};
+
+function getComponentNameForDescriptorType(descriptorData) {
+    return ComponentNames[descriptorData.type];
+}
+
+function getDescription(descriptorData) {
+    return descriptorData.document_description || null;
+}
--- test/store/actions/requestDescriptors.spec.js
+++ test/store/actions/requestDescriptors.spec.js
@@ -3,12 +3,54 @@ const store = require('../../../src/store/store').newStore();
 const actions = require('../../../src/store/actions/actions');
 
 describe(`requestDescriptors action`, () => {
+    before(done => {
+        store
+            .dispatch(actions.ActionNames.RequestDescriptors)
+            .then(_ => done());
+    });
+
     it(`should populate the store with descriptors`, () => {
-        return store.dispatch(actions.ActionNames.RequestDescriptors)
-            .then(_ => {
-                expect(store.state.descriptors).to.have.lengthOf(4);
-                expect(store.state.taxonId).to.equal(1);
-                expect(store.state.taxonTitle).to.equal("Aus bus");
-            });
+        expect(store.state.descriptors).to.have.lengthOf(4);
+        expect(store.state.taxonId).to.equal(1);
+        expect(store.state.taxonTitle).to.equal("Aus bus");
+    });
+
+    it(`should assign a descriptor component based on the type`, () => {
+        const expectedComponentNames = [
+            `qualitative-descriptor`,
+            `presence-descriptor`,
+            `continuous-descriptor`,
+            `sample-descriptor`
+        ];
+
+        store.state.descriptors.forEach((d, index) => {
+            expect(d.componentName).to.equal(expectedComponentNames[index]);
+        });
+    });
+
+    it(`should assign a title`, () => {
+        const expectedTitles = [
+            "Head color",
+            "Big green spine",
+            "Tooth count",
+            "Weight"
+        ];
+
+        store.state.descriptors.forEach((d, index) => {
+            expect(d.title).to.equal(expectedTitles[index]);
+        });
+    });
+
+    it(`should include a description if there is one`, () => {
+        const expectedDescriptions = [
+            "The MARKDOWN description of the descriptor.",
+            null,
+            null,
+            null
+        ];
+
+        store.state.descriptors.forEach((d, index) => {
+            expect(d.description).to.equal(expectedDescriptions[index]);
+        });
     });
 });
