652f2139070c31f2a7beb47f0c4434cb27d184da
Brian Walters <brianw@pixotech.com>
2017-03-30 13:31:59 -0500

    update mock requester to return observations and add REST mock urls to descriptors payload

--- src/request/mockRequest.js
+++ src/request/mockRequest.js
@@ -1,9 +1,25 @@
 const DescriptorsPayload = require('./mockRequests/descriptors.json');
+const Observation1001 = require('./mockRequests/observation-1001.json');
+const Observation1002 = require('./mockRequests/observation-1002.json');
+const Observation1003 = require('./mockRequests/observation-1003.json');
+const Observation1004 = require('./mockRequests/observation-1004.json');
 
 module.exports = {
     getDescriptors() {
         return new Promise(resolve => {
             resolve(DescriptorsPayload);
+        });
+    },
+    getObservationAt(url) {
+        return new Promise(resolve => {
+            if (url === "observation-1001.json")
+                resolve(Observation1001);
+            else if (url === "observation-1002.json")
+                resolve(Observation1002);
+            else if (url === "observation-1003.json")
+                resolve(Observation1003);
+            else if (url === "observation-1004.json")
+                resolve(Observation1004);
         });
     }
 };
