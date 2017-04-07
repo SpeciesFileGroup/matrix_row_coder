c3806bc9e3dae281e2e4c52304c4d8ce623ac6b6
Brian Walters <brianw@pixotech.com>
2017-04-07 10:53:11 -0500

    Set up mock requester with the available mocked endpoints

--- src/request/mockRequest.js
+++ src/request/mockRequest.js
@@ -2,6 +2,30 @@ const MOCK_REQUEST_DIR = `./mockRequests/`;
 
 module.exports = {
     getMatrixRow(url) {
-        return MOCK_REQUEST_DIR + url;
+        return require(MOCK_REQUEST_DIR + url);
+    },
+    getObservation(otuId, descriptorId) {
+        return require(MOCK_REQUEST_DIR + `observations?otu_id=${otuId}&descriptor_id=${descriptorId}.json`);
+    },
+    getDescriptorNotes(descriptorId) {
+        return require(MOCK_REQUEST_DIR + `descriptors/${descriptorId}/notes.json`);
+    },
+    getDescriptorDepictions(descriptorId) {
+        return require(MOCK_REQUEST_DIR + `descriptors/${descriptorId}/depictions.json`);
+    },
+    getObservationNotes(observationId) {
+        return require(MOCK_REQUEST_DIR + `observations/${observationId}/notes.json`);
+    },
+    getObservationDepictions(observationId) {
+        return require(MOCK_REQUEST_DIR + `observations/${observationId}/depictions.json`);
+    },
+    getObservationConfidences(observationId) {
+        return require(MOCK_REQUEST_DIR + `observations/${observationId}/confidences.json`);
+    },
+    getObservationCitations(observationId) {
+        return require(MOCK_REQUEST_DIR + `observations/${observationId}/citations.json`);
+    },
+    getConfidenceLevels() {
+        return require(MOCK_REQUEST_DIR + `confidence_levels.json`);
     }
 };
