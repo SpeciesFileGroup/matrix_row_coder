715311ecb25a701f7a049b6be1866988124c094f
Brian Walters <brianw@pixotech.com>
2017-04-20 10:39:07 -0500

    clean up qualitative descriptor

--- src/MatrixRowCoder/QualitativeDescriptor/QualitativeDescriptor.vue
+++ src/MatrixRowCoder/QualitativeDescriptor/QualitativeDescriptor.vue
@@ -1,16 +1,16 @@
 <template>
-    <ul class="qualitative-descriptor">
-        <li v-for="characterState in descriptor.characterStates">
-            <label>
-                <input
-                    :checked="characterState.isChecked"
-                    @change="test"
-                    type="checkbox" />
+    <div class="qualitative-descriptor">
+        <summary-view v-bind:descriptor="descriptor">
+            <li v-for="characterState in descriptor.characterStates">
+                <label>
+                    <input
+                        type="checkbox" />
 
-                {{ characterState.label }}: {{ characterState.name }}
-            </label>
-        </li>
-    </ul>
+                    {{ characterState.label }}: {{ characterState.name }}
+                </label>
+            </li>
+        </summary-view>
+    </div>
 </template>
 
 <style src="QualitativeDescriptor.styl" lang="stylus"></style>
@@ -18,6 +18,10 @@
 <script>
     const ActionNames = require('../../store/actions/actions').ActionNames;
     const MutationNames = require('../../store/mutations/mutations').MutationNames;
+    const GetterNames = require('../../store/getters/getters').GetterNames;
+
+    const summaryView = require('../SummaryView/SummaryView.vue');
+    const zoomedView = require('../ZoomedView/ZoomedView.vue');
 
     module.exports = {
         name: 'qualitative-descriptor',
@@ -25,12 +29,29 @@
         created: function() {
             const descriptorId = this.$props.descriptor.id;
             const otuId = this.$store.state.taxonId;
-            this.$store.dispatch(ActionNames.RequestObservations, { descriptorId, otuId });
+            this.$store.dispatch(ActionNames.RequestDescriptorDepictions, descriptorId);
+            this.$store.dispatch(ActionNames.RequestDescriptorNotes, descriptorId);
+            this.$store.dispatch(ActionNames.RequestObservations, { descriptorId, otuId })
+                .then(_ => this.$store.getters[GetterNames.GetObservationsFor](descriptorId))
+                .then(observations => {
+                    this.observations = observations;
+                    this.observations.forEach(observation => {
+                        if (observation.id) {
+                            this.$store.dispatch(ActionNames.RequestObservationCitations, observation.id);
+                            this.$store.dispatch(ActionNames.RequestObservationConfidences, observation.id);
+                            this.$store.dispatch(ActionNames.RequestObservationDepictions, observation.id);
+                            this.$store.dispatch(ActionNames.RequestObservationNotes, observation.id);
+                        }
+                    });
+                });
         },
-        methods: {
-            test: function(something) {
-                console.log(something);
-            }
+        data: function() {
+            return {
+                observations: []
+            };
+        },
+        components: {
+            summaryView
         }
     };
 </script>
