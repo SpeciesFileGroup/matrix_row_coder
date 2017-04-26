565d8797e1599905927dc14b6e704d19015e17dc
Brian Walters <brianw@pixotech.com>
2017-04-26 10:20:50 -0500

    A simple JSON getter

---
+++ src/request/request.js
@@ -0,0 +1,23 @@
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open('GET', url, true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400)
+                resolve(JSON.parse(request.responseText));
+            else
+                reject(request.status);
+        };
+
+        request.onerror = function() {
+            reject('An error happened');
+        };
+
+        request.send();
+    });
+}
+
+module.exports = {
+    getJSON
+};
