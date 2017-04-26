9209d6de9c957fc045a47f6920aa9895dda9283c
Brian Walters <brianw@pixotech.com>
2017-04-26 15:01:46 -0500

    demo page up and running with MockRequest

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,7 @@
 const Vue = require('vue');
 const MatrixRowCoder = require('./MatrixRowCoder/MatrixRowCoder.vue');
-const store = require('./store/store').newStore();
+const MockRequest = require('./request/MockRequest');
+const store = require('./store/store').newStore( new MockRequest() );
 
 new Vue({
     el: 'app',
