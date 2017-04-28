be0ac8d909aa3f52a5b51b6007a4d57d9c3c62a5
Brian Walters <brianw@pixotech.com>
2017-04-28 16:21:19 -0500

    refactor remove observation to accept descriptor ids

--- src/store/actions/removeObservation.js
+++ src/store/actions/removeObservation.js
@@ -1,8 +1,27 @@
+const ComponentNames = require('../helpers/ComponentNames');
 const MutationNames = require('../mutations/mutations').MutationNames;
 
-module.exports = function({ commit, state }, observationId) {
+module.exports = function({ commit, state }, args) {
+    const {
+        descriptorId,
+        characterStateId = null
+    } = args;
+
+    const descriptor = state.descriptors.find(d => d.id === descriptorId);
+    const observationId = findObservationId();
+
     return state.request.removeObservation(observationId)
         .then(_ => {
             commit(MutationNames.ClearObservation, observationId);
         });
+
+    function findObservationId() {
+        return state.observations.find(getFindCallback()).id;
+    }
+
+    function getFindCallback() {
+        return descriptor.type === ComponentNames.Qualitative ?
+            o => o.descriptorId === descriptorId && o.characterStateId === characterStateId :
+            o => o.descriptorId === descriptorId;
+    }
 };
--- test/store/actions/removeObservation.spec.js
+++ test/store/actions/removeObservation.spec.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const TestHelpers = require('../../testHelpers');
 const ActionNames = require('../../../src/store/actions/actions').ActionNames;
+const MutationNames = require('../../../src/store/mutations/mutations').MutationNames;
 
 describe(`RemoveObservation action`, () => {
     let store;
@@ -12,29 +13,22 @@ describe(`RemoveObservation action`, () => {
             .then(_ => done());
     });
 
-    it(`should send a request to remove the given observation`, () => {
-        let timesCalled = 0;
+    it(`should send call the request module with the observation id`, () => {
+        const descriptorId = 25;
+        const observationId = 1004;
 
         store.state.request.removeObservation = function(observationId) {
-            timesCalled += 1;
+            expect(observationId).to.equal(1004);
             return Promise.resolve({});
         };
 
-        const observationId = 1001;
-
-        return store.dispatch(ActionNames.RemoveObservation, observationId)
-            .then(_ => {
-                expect(timesCalled).to.equal(1);
-            });
+        return store.dispatch(ActionNames.RemoveObservation, { descriptorId });
     });
 
     it(`should clear the observation`, () => {
-        const descriptorId = 24;
-        const observationId = store.state.observations
-            .filter(o => o.descriptorId === descriptorId)
-            .find(o => o.id).id;
+        const descriptorId = 25;
 
-        return store.dispatch(ActionNames.RemoveObservation, observationId)
+        return store.dispatch(ActionNames.RemoveObservation, { descriptorId })
             .then(_ => {
                 store.state.observations
                     .filter(o => o.descriptorId === descriptorId)
@@ -42,5 +36,38 @@ describe(`RemoveObservation action`, () => {
                         expect(o.id).to.be.null;
                     });
             });
+    });
+
+    it(`observation should not be unsaved`, () => {
+        const descriptorId = 25;
+
+        store.commit(MutationNames.SetPresence, {
+            descriptorId,
+            isChecked: false
+        });
+
+        return store.dispatch(ActionNames.RemoveObservation, { descriptorId })
+            .then(_ => {
+                store.state.observations
+                    .filter(o => o.descriptorId === descriptorId)
+                    .forEach(o => {
+                        expect(o.isUnsaved).to.be.false;
+                    });
+            });
+    });
+
+    describe(`Qualitative Observations`, () => {
+        it(`requires the character state id as an additional arg`, () => {
+            const descriptorId = 24;
+            const characterStateId = 33;
+
+            return store.dispatch(ActionNames.RemoveObservation, { descriptorId, characterStateId })
+                .then(_ => {
+                    const observation = store.state.observations
+                        .find(o => o.descriptorId === descriptorId && o.characterStateId === characterStateId);
+                    expect(observation.isChecked).to.equal(false);
+                    expect(observation.id).to.equal(null);
+                });
+        });
     });
 });
