9641bc6664353128fe16d2cfa65159fa80ad708a
Brian Walters <brianw@pixotech.com>
2017-04-28 13:57:32 -0500

    Create request payloads for sample and continuous observations

--- src/store/actions/updateObservation.js
+++ src/store/actions/updateObservation.js
@@ -1,3 +1,20 @@
-module.exports = function({ state }) {
-    return state.request.updateObservation();
+const ObservationTypes = require('../helpers/ObservationTypes');
+
+module.exports = function({ state }, observationId) {
+    const observation = state.observations.find(o => o.id === observationId);
+
+
+
+    if (observation.type === ObservationTypes.Continuous)
+        return state.request.updateObservation(observationId, {
+            continuous_value: observation.continuousValue,
+            continuous_unit: observation.continuousUnit
+        });
+    else if (observation.type === ObservationTypes.Sample)
+        return state.request.updateObservation(observationId, {
+            sample_n: observation.n,
+            sample_min: observation.min,
+            sample_max: observation.max,
+            sample_units: observation.units
+        });
 };
--- test/store/actions/updateObservation.spec.js
+++ test/store/actions/updateObservation.spec.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const ActionNames = require('../../../src/store/actions/actions').ActionNames;
+const MutationNames = require('../../../src/store/mutations/mutations').MutationNames;
 const TestHelpers = require('../../testHelpers');
 
 describe(`UpdateObservation action`, () => {
@@ -20,9 +21,84 @@ describe(`UpdateObservation action`, () => {
             return Promise.resolve({});
         };
 
-        return store.dispatch(ActionNames.UpdateObservation, 1001)
+        return store.dispatch(ActionNames.UpdateObservation, 1002)
             .then(_ => {
                 expect(timesCalled).to.equal(1);
             });
+    });
+
+    describe(`Continuous Observations`, () => {
+        it(`should include the new continuous value`, () => {
+            const observationId = 1002;
+
+            const expectedContinuousValue = 500;
+
+            store.commit(MutationNames.SetContinuousValue, {
+                descriptorId: 26,
+                continuousValue: expectedContinuousValue
+            });
+
+            store.state.request.updateObservation = function(oIdArg, payload) {
+                expect(oIdArg).to.equal(observationId);
+                expect(payload.continuous_value).to.equal(expectedContinuousValue);
+            };
+
+            return store.dispatch(ActionNames.UpdateObservation, observationId);
+        });
+
+        it(`should include the new continuous unit`, () => {
+            const observationId = 1002;
+
+            const expectedContinuousUnit = "cm";
+
+            store.commit(MutationNames.SetContinuousUnit, {
+                descriptorId: 26,
+                continuousUnit: expectedContinuousUnit
+            });
+
+            store.state.request.updateObservation = function(oIdArg, payload) {
+                expect(oIdArg).to.equal(observationId);
+                expect(payload.continuous_unit).to.equal(expectedContinuousUnit);
+            };
+
+            return store.dispatch(ActionNames.UpdateObservation, observationId);
+        });
+    });
+
+    describe(`Sample Observations`, () => {
+        it(`should include the sample properties`, () => {
+            const observationId = 1003;
+            const descriptorId = 27;
+
+            store.commit(MutationNames.SetSampleMinFor, {
+                descriptorId,
+                min: 5
+            });
+
+            store.commit(MutationNames.SetSampleMaxFor, {
+                descriptorId,
+                max: 10
+            });
+
+            store.commit(MutationNames.SetSampleNFor, {
+                descriptorId,
+                n: 200
+            });
+
+            store.commit(MutationNames.SetSampleUnitFor, {
+                descriptorId,
+                units: 'feet'
+            });
+
+            store.state.request.updateObservation = function(oIdArg, payload) {
+                expect(oIdArg).to.equal(observationId);
+                expect(payload.sample_min).to.equal(5);
+                expect(payload.sample_max).to.equal(10);
+                expect(payload.sample_n).to.equal(200);
+                expect(payload.sample_units).to.equal(`feet`);
+            };
+
+            store.dispatch(ActionNames.UpdateObservation, observationId);
+        });
     });
 });
