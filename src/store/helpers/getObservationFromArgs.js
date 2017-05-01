33045a4add5c03a40a51920ab52bb4073f5e5909
Brian Walters <brianw@pixotech.com>
2017-05-01 11:01:41 -0500

    get observation helper

---
+++ src/store/helpers/getObservationFromArgs.js
@@ -0,0 +1,17 @@
+const ComponentNames = require('../helpers/ComponentNames');
+
+module.exports = function(state, args) {
+    const {
+        descriptorId,
+        characterStateId = null
+    } = args;
+
+    const descriptor = state.descriptors.find(d => d.id === descriptorId);
+
+    if (descriptor.componentName === ComponentNames.Qualitative)
+        return state.observations.find(o => {
+            return o.characterStateId === characterStateId && o.descriptorId === descriptorId;
+        });
+    else
+        return state.observations.find(o => o.descriptorId === descriptorId);
+};
