774fce42c523a00fb88e71e9ca623b09ef43c829
Matt Yoder <diapriid@gmail.com>
2017-05-04 11:38:01 -0500

    extract config to json, update vue

--- src/entry-live.js
+++ src/entry-live.js
@@ -1,22 +1,16 @@
+const ApiConfig = require('./api-config.json');
 const Vue = require('vue');
 const MatrixRowCoder = require('./MatrixRowCoder/MatrixRowCoder.vue');
 const MatrixRowCoderRequest = require('./request/MatrixRowCoderRequest');
 const store = require('./store/store').newStore( new MatrixRowCoderRequest() );
+
 
 new Vue({
     el: 'app',
     store,
     render: function (createElement) {
         return createElement(MatrixRowCoder, {
-            props: {
-                matrixId: 1,
-                otuId: 1,
-                apiBase: 'http://127.0.0.1:3000/api/v1',
-                apiParams: {
-                    project_id: 1,
-                    token: `T2LDAEltkiZ27wqBgwPJ9w`
-                }
-            }
+            props: ApiConfig            
         });
     }
 });
